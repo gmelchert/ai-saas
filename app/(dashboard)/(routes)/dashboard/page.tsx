deee222ce79fc86ba19c21409256e17ae94cb461
Gabriel Fernandes Melchert <Gabriel.Fernandes.Melchert@ibm.com>
2023-09-11 21:43:47 -0300

    first commit

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
+
+export default function DashboardPage() {
+  return (
+    <div>
+      <p className=' text-green-400'>hello ai saas</p>
+    </div>
+  )
+}
